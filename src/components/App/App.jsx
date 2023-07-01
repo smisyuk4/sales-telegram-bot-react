798407c5937ea0c9c10417822103a2e88b1e2f86
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-07-02 00:07:36 +0300

    clear some code

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,10 @@
 import { Form } from '../Form';
-import { useTelegram } from '../../hooks/telegramHook';
 import { Container, Title } from './App. styled';
 
 export const App = () => {
-  const { user, onClose } = useTelegram();
   return (
     <Container>
       <Title> Оголошення </Title>
-      <p>Привіт {user}</p>
-
-      <button onClick={onClose} type="button">
-        Вийти
-      </button>
       <Form />
     </Container>
   );
--- src/salesApi.js
+++ src/salesApi.js
@@ -2,13 +2,7 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://telegram-bot-d339c.ew.r.appspot.com';
 
 export const salesApi = async (path, data) => {
-  return await axios
-    .post(path, data, {
-      // headers: {
-      //   'Content-Type': 'multipart/form-data',
-      // },
-    })
-    .then(response => {
-      return response.data;
-    });
+  return await axios.post(path, data).then(response => {
+    return response.data;
+  });
 };
