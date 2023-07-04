01ae567963635365a824317daab1b96dc9b97f4a
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-07-04 23:30:45 +0300

    cors

--- src/salesApi.js
+++ src/salesApi.js
@@ -2,7 +2,24 @@ import axios from 'axios';
 axios.defaults.baseURL = 'https://telegram-bot-d339c.ew.r.appspot.com';
 
 export const salesApi = async (path, data) => {
-  return await axios.post(path, data).then(response => {
-    return response.data;
-  });
+  // return fetch('https://telegram-bot-d339c.ew.r.appspot.com' + path, {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify(data),
+  // });
+
+  return await axios
+    .post(path, JSON.stringify(data), {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(response => {
+      return response.data;
+    })
+  // .catch(error => {
+  //   console.log('salesApi ===> ', error);
+  // });
 };
