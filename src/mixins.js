6fec9f154a2b50f74908b9583162f5b63cb20335
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-07-08 22:48:53 +0300

    fix button styles

--- src/mixins.js
+++ src/mixins.js
@@ -12,21 +12,21 @@ export const errorMessage = () => {
 
 export const bigButton = () => {
   return `padding: 10px 15px;
-  width: 100%;
+    width: 100%;
 
-  font-size: 18px;
-  line-height: 1.2;
+    font-size: 18px;
+    line-height: 1.2;
+    color: black;
 
-  border-radius: 12px;
-  border: 1px solid;
-  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
-    rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
-  cursor: pointer;
+    border-radius: 12px;
+    border: 1px solid black;
+    box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+      rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+    cursor: pointer;
 
-  &:active {
-    background-color: black;
-    color: white;
-  }
-  
+    &:active {
+      background-color: black;
+      color: white;
+    }
   `;
 };
