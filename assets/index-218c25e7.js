d282f23ac27fba139a5b7ce5161ef637a4da96ba
smisyuk4 <smisyuk4@users.noreply.github.com>
2023-07-08 19:49:51 +0000

    deploy: 6fec9f154a2b50f74908b9583162f5b63cb20335

--- assets/index-218c25e7.js
+++ assets/index-218c25e7.js
@@ -49,22 +49,22 @@ Error generating stack: `+o.message+`
     align-items: center;`,Xi=()=>`font-size: 14px;
     font-style: italic;
     color: red;`,Sc=()=>`padding: 10px 15px;
-  width: 100%;
+    width: 100%;
 
-  font-size: 18px;
-  line-height: 1.2;
+    font-size: 18px;
+    line-height: 1.2;
+    color: black;
 
-  border-radius: 12px;
-  border: 1px solid;
-  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
-    rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
-  cursor: pointer;
+    border-radius: 12px;
+    border: 1px solid black;
+    box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+      rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+    cursor: pointer;
 
-  &:active {
-    background-color: black;
-    color: white;
-  }
-  
+    &:active {
+      background-color: black;
+      color: white;
+    }
   `,bw=pe.div`
   h2 {
     margin-bottom: 5px;
