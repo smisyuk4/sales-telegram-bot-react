c37cdf18926978caa91139aa7fd5c072b6aae88e
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-07-16 00:05:49 +0300

    HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { App } from './components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/sales-telegram-bot-react">
-    <React.StrictMode>
+  // <BrowserRouter basename="/sales-telegram-bot-react">
+  <React.StrictMode>
+    <HashRouter>
       <App />
-    </React.StrictMode>
-  </BrowserRouter>
+    </HashRouter>
+  </React.StrictMode>
+  // </BrowserRouter>
 );
