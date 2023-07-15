fbd0e368e7e6d3c8b3f382afc749d4c18b77a78d
smisyuk4 <smisyuk4@users.noreply.github.com>
2023-07-15 17:32:06 +0000

    deploy: fd734a89cb949865a03a84cf0dc4048b65290df3

---
+++ assets/buyPage-6aaffcfd.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-0fba1107.js";const t=()=>e.jsx(e.Fragment,{children:e.jsx("p",{children:"buy page"})});export{t as default};
---
+++ assets/rulsPage-16b94a0c.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-0fba1107.js";import{R as s}from"./Ruls-73320241.js";import"./emotion-styled.browser.esm-df88bc91.js";const a=()=>r.jsx(s,{});export{a as default};
