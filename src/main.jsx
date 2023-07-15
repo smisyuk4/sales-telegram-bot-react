b82d5cc5ce28190235cc1301894d4c03885f2f17
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-07-16 00:22:56 +0300

    add 404 page for gh page

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from './components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <BrowserRouter basename="/sales-telegram-bot-react">
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/sales-telegram-bot-react">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
-  // </BrowserRouter>
 );
