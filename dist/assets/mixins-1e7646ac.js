50b2b24415b890053259b3dd954a94d86a8fd690
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-07-15 23:34:17 +0300

    Adding dist

---
+++ dist/assets/mixins-1e7646ac.js
@@ -0,0 +1,22 @@
+const r=()=>`display: flex;
+    justify-content: center;
+    align-items: center;`,e=()=>`font-size: 14px;
+    font-style: italic;
+    color: red;`,o=()=>`padding: 10px 15px;
+    width: 100%;
+
+    font-size: 18px;
+    line-height: 1.2;
+    color: black;
+
+    border-radius: 12px;
+    border: 1px solid black;
+    box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+      rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+    cursor: pointer;
+
+    &:active {
+      background-color: black;
+      color: white;
+    }
+  `;export{o as b,e,r as f};
