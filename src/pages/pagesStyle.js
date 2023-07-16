b836a0bd4d2f545beb8596b544dbd2b3b3619053
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-07-16 11:05:23 +0300

    Update pagesStyle.js

--- src/pages/pagesStyle.js
+++ src/pages/pagesStyle.js
@@ -21,3 +21,15 @@ export const ErrorStyled = styled(HomeStyled)`
   background-size: auto;
   height: 100vh;
 `;
+
+
+export const TitleStyle = styled.h1`
+    padding: 5px;
+  text-align: center;
+  background-color: white;
+  border-radius: 12px;
+
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+    rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+`;
+`;
