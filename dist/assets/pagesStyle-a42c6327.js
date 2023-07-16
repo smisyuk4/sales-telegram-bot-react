6d1b44ac794956a82d4f7ff8f65a592ba2e132a7
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-07-16 19:01:26 +0300

    Adding dist

---
+++ dist/assets/pagesStyle-a42c6327.js
@@ -0,0 +1,24 @@
+import{n as e}from"./emotion-styled.browser.esm-3c8eb009.js";import{f as r}from"./mixins-1e7646ac.js";const a="/sales-telegram-bot-react/assets/wall-a0138653.jpeg",o="/sales-telegram-bot-react/assets/errorIcon-f343573f.svg",t=e.div`
+  ${r}
+  align-items: flex-start;
+  padding-top: 70px;
+
+  background-image: url(${a});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+
+  height: 600px;
+`,p=e(t)`
+  background-image: url(${o});
+  background-size: auto;
+  height: 100vh;
+`,g=e.h1`
+  padding: 5px;
+  text-align: center;
+  background-color: white;
+  border-radius: 12px;
+
+  box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
+    rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
+`;export{p as E,t as H,g as T};
