c544cc5ef621f9a6a1435934aabb5b053a24fdba
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-07-19 22:00:46 +0300

    env

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,9 +1,10 @@
 import { Notify } from '../components/Notify';
 import { HomeStyled } from './pagesStyle';
-const { VITE_CHANNEL_ID } = import.meta.env;
+const { VITE_CHANNEL_ID, HUB_BASE_URL } = import.meta.env;
 
 const HomePage = () => {
-  console.log(VITE_CHANNEL_ID);
+  console.log('VITE_CHANNEL_ID = ', VITE_CHANNEL_ID);
+  console.log('HUB_BASE_URL = ', HUB_BASE_URL);
   return (
     <HomeStyled>
       <Notify />
