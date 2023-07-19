0cf9d2ead66180a3f0862c0efa6d9e2e7580cbf0
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-07-19 20:32:40 +0300

    add .env

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,7 +1,9 @@
 import { Notify } from '../components/Notify';
 import { HomeStyled } from './pagesStyle';
+const { VITE_CHANNEL_ID } = import.meta.env;
 
 const HomePage = () => {
+  console.log(VITE_CHANNEL_ID);
   return (
     <HomeStyled>
       <Notify />
