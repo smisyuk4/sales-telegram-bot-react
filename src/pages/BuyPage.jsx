289c51a647f6ed1fa24248c3faa5ae974ac1291b
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-07-22 22:25:05 +0300

    msg permission

--- src/pages/BuyPage.jsx
+++ src/pages/BuyPage.jsx
@@ -2,11 +2,13 @@ import { useState, useEffect } from 'react';
 import { useTelegram } from '../hooks/telegramHook';
 import { BuyForm } from '../components/BuyForm/BuyForm';
 import { getDatafromDb, checkPermission } from '../firebase/services';
+import { MessageVisit } from './pagesStyle';
 
 const BuyPage = () => {
   const { user, onClose, queryId } = useTelegram();
   // const [data, setData] = useState([]);
   const [permissionMsg, setPermissionMsg] = useState({});
+  const [isShowAlert, setIsShowAlert] = useState(false);
 
   useEffect(() => {
     const get = async () => {
@@ -26,13 +28,23 @@ const BuyPage = () => {
   }
 
   if (permissionMsg.permission === false) {
-    return <p>{permissionMsg.text}</p>;
+    setIsShowAlert(true);
+
+    return <MessageVisit>{permissionMsg.text}</MessageVisit>;
   }
 
   if (permissionMsg.permission === true) {
+    setIsShowAlert(true);
+
+    const timerId = setTimeout(() => {
+      setIsShowAlert(false);
+      clearTimeout(timerId);
+    }, 1500);
+
     return (
       <>
-        <p>{permissionMsg.text}</p>
+        {isShowAlert && <MessageVisit>{permissionMsg.text}</MessageVisit>}
+
         <BuyForm user={user} queryId={queryId} onClose={onClose} />
       </>
     );
--- src/pages/pagesStyle.js
+++ src/pages/pagesStyle.js
@@ -31,3 +31,9 @@ export const TitleStyle = styled.h1`
   box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px,
     rgba(60, 64, 67, 0.15) 0px 2px 6px 2px;
 `;
+
+export const MessageVisit = styled.p`
+  font-size: 14px;
+  font-style: italic;
+  color: blue;
+`;
