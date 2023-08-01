580ff7630c8dd20ad2e92286b054522ce4edb6eb
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-08-01 23:29:43 +0300

    deploy prod

--- src/salesApi.js
+++ src/salesApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// axios.defaults.baseURL = 'https://telegram-bot-d339c.ew.r.appspot.com';
-axios.defaults.baseURL = 'https://sales-telegram-bot-node-dev-test.onrender.com'; // test server
+axios.defaults.baseURL = 'https://telegram-bot-d339c.ew.r.appspot.com';
+// axios.defaults.baseURL = 'https://sales-telegram-bot-node-dev-test.onrender.com'; // test server
 
 export const salesApi = async (path, data, config) => {
   return await axios.post(path, data, config).then(response => {
