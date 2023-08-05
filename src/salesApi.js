19dc6815b13ad43490efda91460aff7241a3b0d0
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2023-08-05 23:23:46 +0300

    add links to server

--- src/salesApi.js
+++ src/salesApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// axios.defaults.baseURL = 'https://telegram-bot-d339c.ew.r.appspot.com';
-axios.defaults.baseURL = 'https://sales-telegram-bot-node-dev-test.onrender.com'; // test server
+// axios.defaults.baseURL = 'https://telegrambotnode-394718.ey.r.appspot.com'; // production server
+axios.defaults.baseURL = 'https://sales-telegram-bot-node.onrender.com'; // test server
 
 export const salesApi = async (path, data, config) => {
   return await axios.post(path, data, config).then(response => {
